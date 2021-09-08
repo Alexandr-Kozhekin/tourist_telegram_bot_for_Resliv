65d96fd1f313812279703933ca879bfba2920a68
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2021-09-08 22:07:00 +0300

    add starter application

---
+++ src/main/java/telegram_bot/application/TouristTelegramBotApplication.java
@@ -0,0 +1,22 @@
+package telegram_bot.application;
+
+import telegram_bot.web_ui.web_config.SpringWebConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class TouristTelegramBotApplication {
+
+    public static void main(String[] args) {
+
+        try {
+            ConfigurableApplicationContext context = SpringApplication.run(SpringWebConfiguration.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
